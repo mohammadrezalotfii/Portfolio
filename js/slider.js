068871853c8025fc0080075b4efffa26354fa181
MohammadrezaLotfi <mohammadrezaLottfii@gmail.com>
2025-03-16 17:26:20 +0330

    Blog section created and fix some bugs in mobile version

--- js/slider.js
+++ js/slider.js
@@ -1,34 +1,34 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 24,
-    loop: true,
-    autoplay: {
-      delay: 10000,
+  slidesPerView: 2,
+  spaceBetween: 24,
+  loop: true,
+  autoplay: {
+    delay: 10000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5,
+      spaceBetween: 24,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 24,
     },
-    breakpoints: {
-      0: {
-        slidesPerView: 2,
-        spaceBetween: 24,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 24,
-      },
-      1025: {
-        slidesPerView: 2,
-        spaceBetween: 24,
-      },
-      1300: {
-        slidesPerView: 3,
-        spaceBetween: 24,
-      },
-      1440: {
-        slidesPerView: 3,
-        spaceBetween: 24,
-      },
+    1025: {
+      slidesPerView: 2,
+      spaceBetween: 24,
     },
-  });
+    1300: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+});
